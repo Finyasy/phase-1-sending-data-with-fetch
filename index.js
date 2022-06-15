1810978a7028095608aaa7c11b01e86438031865
Bryan Bosire <bryanbosire@gmail.com>
2022-06-15 23:44:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+const submitData = () => {
+  const userData ={name : "Steve",email : "steve@steve.com"}
+
+  return fetch("http://localhost:3000/users",{
+    method : "POST",
+    headers : {
+      "content-Type":"application/json",
+      Accept : "application/json",
+    },
+    body:JSON.stringify(userData)
+  })
+  .then(res => res.json())
+  .then(data =>{
+    document.body.innerHTML = data.id
+  })
+  .catch(err => {
+    document.body.innerHTML = err.message
+    alert("incorrect!")
+  })
+}
